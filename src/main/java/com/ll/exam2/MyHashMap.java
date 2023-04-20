bb101512c9f327f4b791dcb1bd053036a401aad1
yurrrrri <yu_ri_son@naver.com>
2023-04-20 15:03:05 +0900

    MyHashMap 구현 완료

--- src/main/java/com/ll/exam2/MyHashMap.java
+++ src/main/java/com/ll/exam2/MyHashMap.java
@@ -2,18 +2,26 @@ package com.ll.exam2;
 
 public class MyHashMap<K, V> {
 
-    private Object[] keys;
-    private Object[] values;
+    private Entry[] entries;
 
     private int size = 0;
+
+    public static class Entry<K, V> {
+        K key;
+        V value;
+
+        public Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
 
     public MyHashMap() {
         this(2);
     }
 
     public MyHashMap(int dataLength) {
-        keys = new Object[dataLength];
-        values = new Object[dataLength];
+        entries = new Entry[dataLength];
     }
 
     public int size() {
@@ -21,11 +29,40 @@ public class MyHashMap<K, V> {
     }
 
     public V put(K key, V value) {
-        keys[size] = key;
-        values[size] = value;
+        int indexOfKey = indexOfKey(key);
+
+        if(indexOfKey != -1) {
+            V oldValue = (V) entries[indexOfKey].value;
+            entries[indexOfKey].value = value;
+            return oldValue;
+        }
+
+        makeNewArrayIfFull();
+
+        entries[size] = new Entry<>(key, value);
 
         size++;
         return null;
+    }
+
+    private void makeNewArrayIfFull() {
+        if(isFull()) {
+            makeNewArray();
+        }
+    }
+
+    private boolean isFull() {
+        return size >= entries.length;
+    }
+
+    private void makeNewArray() {
+        Entry[] newEntries = new Entry[entries.length * 2];
+
+        for(int i = 0; i < entries.length; i++){
+            newEntries[i] = entries[i];
+        }
+
+        entries = newEntries;
     }
 
     public V get(K key) {
@@ -33,13 +70,54 @@ public class MyHashMap<K, V> {
 
         if(indexOfKey == -1) return null;
 
-        return (V) values[indexOfKey];
+        return (V) entries[indexOfKey].value;
     }
 
     private int indexOfKey(K key) {
         for(int i = 0; i < size; i++) {
-            if(key.equals(keys[i])) return i;
+            if(key.equals(entries[i].key)) return i;
         }
         return -1;
+    }
+
+    private int indexOfValue(V value) {
+        for(int i = 0; i < size; i++) {
+            if(value.equals(entries[i].value)) return i;
+        }
+        return -1;
+    }
+
+    public V remove(K key) {
+        int index = indexOfKey(key);
+
+        if(index == -1) return null;
+
+        V oldValue = (V) entries[index].value;
+
+        for(int i = index + 1; i < size; i++) {
+            entries[i - 1] = entries[i];
+        }
+
+        size--;
+        return oldValue;
+    }
+
+    public boolean containsKey(K key) {
+        return indexOfKey(key) != -1;
+    }
+
+    public boolean containsValue(V value) {
+        return indexOfValue(value) != -1;
+    }
+
+    public void clear() {
+        for(int i = 0; i < size; i++) {
+            entries[i] = null;
+        }
+        size = 0;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
     }
 }
--- src/test/java/com/ll/exam2/MyHashMapTest.java
+++ src/test/java/com/ll/exam2/MyHashMapTest.java
@@ -36,4 +36,70 @@ public class MyHashMapTest {
         assertThat(map.get("S2")).isEqualTo(23);
     }
 
+    @Test
+    @DisplayName("배열 크기 자동 증가")
+    void t004() {
+        MyHashMap<String, Integer> map = new MyHashMap<>();
+        map.put("S1", 22);
+        map.put("S2", 23);
+        map.put("S3", 24);
+    }
+
+    @Test
+    @DisplayName("put()")
+    void t005() {
+        MyHashMap<String, Integer> map = new MyHashMap<>();
+        map.put("S1", 22);
+        map.put("S2", 23);
+
+        assertThat(map.put("S1", 24)).isEqualTo(22);
+        assertThat(map.size()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("remove()")
+    void t006() {
+        MyHashMap<String, Integer> map = new MyHashMap<>();
+        map.put("S1", 22);
+        map.put("S2", 23);
+
+        assertThat(map.remove("S1")).isEqualTo(22);
+        assertThat(map.remove("S3")).isNull();
+        assertThat(map.size()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("containsKey()")
+    void t007() {
+        MyHashMap<String, Integer> map = new MyHashMap<>();
+        map.put("S1", 22);
+        map.put("S2", 23);
+
+        assertThat(map.containsKey("S1")).isTrue();
+        assertThat(map.containsKey("S3")).isFalse();
+    }
+
+    @Test
+    @DisplayName("containsValue()")
+    void t008() {
+        MyHashMap<String, Integer> map = new MyHashMap<>();
+        map.put("S1", 22);
+        map.put("S2", 23);
+
+        assertThat(map.containsValue(22)).isTrue();
+        assertThat(map.containsValue(24)).isFalse();
+    }
+
+    @Test
+    @DisplayName("clear()")
+    void t009() {
+        MyHashMap<String, Integer> map = new MyHashMap<>();
+        map.put("S1", 22);
+        map.put("S2", 23);
+        map.clear();
+
+        assertThat(map.size()).isEqualTo(0);
+        assertThat(map.isEmpty()).isTrue();
+    }
+
 }
